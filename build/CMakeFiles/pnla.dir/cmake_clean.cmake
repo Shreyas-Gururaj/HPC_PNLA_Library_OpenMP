7a00fa58cd2541523e6a953a3100a5e2d4860233
Shreyas-Gururaj <bt717775@myubt.de>
2021-03-17 17:55:11 +0100

    First commit

---
+++ build/CMakeFiles/pnla.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pnla.dir/src/vector_seq.cpp.o"
+  "libpnla.pdb"
+  "libpnla.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pnla.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nuss/75/bt717775/PNLA_Template/tests
+# Build directory: /home/nuss/75/bt717775/PNLA_Template/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test_vectors_default "/home/nuss/75/bt717775/PNLA_Template/build/tests/test_vectors")
+add_test(Test_vectors_dim_20 "/home/nuss/75/bt717775/PNLA_Template/build/tests/test_vectors" "20")
