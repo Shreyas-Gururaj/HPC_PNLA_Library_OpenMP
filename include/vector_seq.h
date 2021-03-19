91515e455a76e001117213f467f69e19aefc51e1
Shreyas-Gururaj <bt717775@myubt.de>
2021-03-19 17:51:15 +0100

    Problem 4 Sequential vector routines

--- include/vector_seq.h
+++ include/vector_seq.h
@@ -13,12 +13,66 @@
 #ifndef __VECTOR_SEQ_H__
 #define __VECTOR_SEQ_H__
 
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 /// put everything of the PNLA-library into the suitable namespace
 namespace pnla{
-    
+
     /// Start here with the defenition of your struct/class for storing a vector 
     /// of doubles
+
+    class Vector_n_dim
+    {
+        public:
+
+            //
+            Vector_n_dim() = delete;
+
+            
+             Vector_n_dim(const unsigned int dimension)
+             {
+                 vector_dimension= dimension;
+                 x.assign(vector_dimension, 1.0);
+             }
+
+            //
+            int get_vector_dimension() 
+            { 
+             return vector_dimension; 
+            }
+
+            //
+            std::vector <double> get_init_vector()
+            {
+             return x;
+            }
+
+
+            //
+            void inititalize_constant_elements(std::vector<double> &init_vector, const double constant_value);
+
+            //
+            void inititalize_range_elements(std::vector<double> &init_vector);
+
+            //
+            //gitvoid inititalize_std_doubles(std::vector<double> &init_vector);
+
+        private:
+
+            //
+            unsigned int vector_dimension;
+
+            //
+            std::vector <double> x;
+
+
+    };
+
+
 }//end namespace pnla
 
 #endif // __VECTOR_SEQ_H__
--- src/vector_seq.cpp
+++ src/vector_seq.cpp
@@ -13,7 +13,32 @@
 
 namespace pnla{
 
+
     /// Here is the place for the actual implementation of your sequential 
     /// vector routines 
+
+    void Vector_n_dim::inititalize_constant_elements(std::vector<double> &init_vector, const double constant_value)
+
+    {
+        for(int elem_index = 0; elem_index < init_vector.size(); elem_index++)
+        {
+            init_vector[elem_index] = constant_value;
+        }
+    }
+
+    //
+    void Vector_n_dim::inititalize_range_elements(std::vector<double> &init_vector)
+    {
+        for(int elem_index = 0; elem_index < init_vector.size(); elem_index++)
+        {
+            init_vector[elem_index] = elem_index;
+        }
+    }
+
+    //
+    // void Vector_n_dim::inititalize_std_doubles(std::vector<double> &init_vector)
+    // {
+
+    // }
 
 } // end namespace pnla
